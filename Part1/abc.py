3c50a09fdc1ffbd770bbfe169e500c0525166b1b
Sajaldeb-bs23 <Sajal.Debnath@brainstation-23.com>
2022-07-18 15:49:30 +0600

    for customer list upload function.

---
+++ Part1/abc.py
@@ -0,0 +1,34 @@
+var = 10                    # Second Example
+while var > 0:              
+    print( 'Current variable value :', var)
+    var = var -1
+    if var == 5:
+       continue
+    if var == 4:
+       continue
+    print("Sajal")
+
+print("Good bye!")
+
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
+print(thisdict.get('Name'))
+
+q = 5
+p= 5
+r = 4
+
+if q is 3:
+    print("sajal")
+elif p is 4:
+    print("Debnath")
+elif r is 4:
+    print("Debnath2")
+
